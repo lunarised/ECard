e62c490f84dbe75d90e72295d3bf63e5dd6b751d
lunarised <j@p7.co.nz>
2019-08-28 05:11:45 +1200

    HumanCardCheckingDone

--- ECard.c
+++ ECard.c
@@ -1,9 +1,11 @@
 #include <stdlib.h>
+#include <ctype.h>
 #include <stdio.h>
-
+char side;
 int main(void){
 	printf("%s", "Howdy");
 	setup();
+	cardSelect();
 	return EXIT_SUCCESS;
 }
 int compare(char p1, char p2){
@@ -21,20 +23,48 @@ int compare(char p1, char p2){
 	}	
 }
 int setup(void){
-	char side = 'x';
+	side = 'Z';
 	int validSelection = 0;
 	while (validSelection == 0){
-	printf("%s", "What side do you wish to play? Emperor or Slave?\n");
-	scanf(" %c", &side);
-	if (side == 's' || side == 'e' || side == 'S' || side == 'E'){
-		validSelection = 1;
-	}
-	else{
-		printf("%c%s", side, "d\n");
-		printf("%s", "Invalid Selection. Please choose again!");
-	}
+		printf("%s", "What side do you wish to play? Emperor or Slave?\n");
+		scanf(" %c", &side);
+		if (toupper(side) == 'S' || toupper(side) == 'E'){
+			validSelection = 1;
+		}
+		else{
+			printf("%c%s", side, "d\n");
+			printf("%s", "Invalid Selection. Please choose again!");
+		}
 
 	}
+	side = toupper(side);
+	if (side == 'S'){
+		printf("%s", "You have chosen to play with the slave!");
+		
+	}else{
 
-printf("%s", "fools");
+		printf("%s", "You have chosen to play with the emperor!");
+	}
+
+}
+void cardSelect(void){
+	int validSelection = 0;
+	char card;
+	while(validSelection == 0){
+	printf("%s", "Select your card, using C for Citizen, S for slave or E for emperor");
+	scanf(" %c", &card);
+	card = toupper(card);
+	if (card == 'S' || card == 'E' || card == 'C'){
+		printf("%s" , "Moo");
+		if (!((card == 'S' && side == 'E')||(card == 'E' && side == 'S'))){
+			printf("%s", "Meow");
+			validSelection = 1;
+	}
+		else{
+			"Invalid Selection";
+		}
+	}
+//	 (toupper(side) ==  )
+	}
+//	return 'e';
 }
